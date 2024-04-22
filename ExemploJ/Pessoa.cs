09d1d4450355667f417b7a2d6c966176ea8f2c7e
Malone <lucas.oliveira1720@etec.sp.gov.br>
2024-04-22 20:19:15 -0300

    first commit

---
+++ ExemploJ/Pessoa.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploJ
+{
+    class Pessoa
+    {
+             private string nome;
+        private double salario;
+
+        public double Salario
+        {
+            get
+            {
+                return this.salario;
+            }
+            set
+            {
+                if (this.salario > value)
+                {
+                    Console.WriteLine("Salario atual maior que o informado");
+                }
+                else
+                {
+                    this.salario = value;
+                    Console.WriteLine("Salario Atualizado");
+                }
+            }
+        }
+        public Pessoa(string nome, double salario)
+        {
+            this.nome = nome;
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return this.nome.ToUpper();
+            }
+            set
+            {
+                this.nome = value;
+            }
+        }
+
+        public string Email { get; set; }
+    }
+}
---
+++ ExemploJ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa("Kastel",1100);
+            //p.Nome = "Kastel";
+            Console.WriteLine("nome: {0}", p.Nome);
+            p.Salario = 1001;
+            p.Salario = 900;
+            p.Salario = 1101;
+            Console.WriteLine("salario {0}", p.Salario);
+            Console.ReadKey();
+        }
+    }
+}
